3d766e124e850daee02ac72752cef4b85ce56a41
Amelia Holloway <57925045+AkinaMisaki@users.noreply.github.com>
2025-04-29 19:42:41 -0300

    Melhoria no código
    Redundâncias foram removidas e foi melhorada a eficiência do código, além da remoção de bugs e resultados inesperados com símbolos estranhos (como emojis, números e outros)

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,30 +1,57 @@
-alfab = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' # Variável referência do alfabeto
-crypt = "" # Variável do resultado final
-texto = input("Insira o texto: ")
-rotat = int(input("Insira quantas posições será movido: (Positivo = Direita; Negativo = Esquerda): "))
+import os
 
-for x in range(0, len(texto)): # Repete o loop até alcançar o fim do texto
+def clear(): # Função para limpar a tela
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-    texlw = texto.lower() # Transforma o número em minúsculo para ter consistência de valores da tabela ASCII
+def caesar(): # Função principal
 
-    if (ord(texlw[x])) == 32: # Checa se o caractére é o espaço, se sim, adiciona no string e começa outro loop FOR
-        letra = " " 
-        crypt += letra 
+    crypt = "" # Váriável do resultado final
+
+    for char in texto: # Repete o loop até alcançar o fim do texto
+
+        if (ord(char)) == 32: # Checa se o caractére é o espaço, se sim, adiciona no string e começa outro loop FOR
+            crypt += " "
+            continue
+
+        numlt = ord(char) + rotat # Transforma o número dos caractéres da tabela ASCII e adiciona o número de rotações
+
+
+        if char.isupper() == True: # Verifica se é uma letra maiúscula e garante que fique dentro do alfabeto de A-Z
+            if numlt >= 90:
+                numlt -= 26
+            elif numlt < 65:
+                numlt += 26
+        elif char.islower() == True: # Verifica se é uma letra minúscula e garante que fique dentro do alfabeto de a-z
+            if numlt >= 123: 
+                numlt -= 26
+            elif numlt < 97: 
+                numlt += 26
+        else: # Verifica se é um número, se sim, transforma de volta ao caractére original
+            numlt -= rotat
+            
+        crypt += chr(numlt) # Concatena a letra à string crypt, encriptando ou decriptando a variável
+    
+    return crypt # Retorna a variável crypt para ser utilizada
+
+while True: # Roda o código até o usuário decidir parar
+
+    texto = input("Insira o texto:\n")
+    rotat = int(input("\nInsira quantas posições será movido: (Positivo = Direita; Negativo = Esquerda):\n"))
+
+    resul = caesar()
+    print(f"\nResultado:\n{resul}\n")
+
+    saida = input('Aperte enter para continuar ou digite "Sair" para fechar o programa\n')
+
+    if saida.lower() == "sair":
+        exit()
+    else:
+        clear()
         continue
-
-    numlt = ord(texlw[x]) - 97 # Transforma o número dos caractéres da tabela ASCII para 0-25 (1-26)
-    numlt += rotat # Adiciona a quantidade de rotações/movimentações da cifra ao número
-
-    if numlt >= 26: #Verifica se o programa está tentando ler algo após de Z, se sim, volta para A
-        numlt -= 26
-    elif numlt < 0: #Verifica se o programa está tentando ler algo antes de A, se sim, vai para Z
-        numlt += 26
-
-    letra = alfab[numlt] # Coloca o caractére equivalente do número na variável letra
-    crypt += letra # Concatena a letra à string crypt, encriptando ou decriptando a variável
-
-print(crypt)
 
 # Exemplo utilizado pelo professor em sala de aula:
 # Suhpdwxuh rswlplcdwlrq lv wkh urrw ri doo hylo
 # PREMATURE OPTIMIZATION IS THE ROOT OF ALL EVIL
+
+# String teste
+# ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 0123456789 ' ! @ # $ % & * ( ) _ - + =
